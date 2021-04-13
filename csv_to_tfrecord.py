05ad1386a254dfdd915656b88a20157689eb3b8c
GiX7000 <55018220+GiX7000@users.noreply.github.com>
2021-04-13 18:20:46 +0300

    Update csv_to_tfrecord.py

--- csv_to_tfrecord.py
+++ csv_to_tfrecord.py
@@ -2,9 +2,10 @@
 Usage:
   # From tensorflow/models/
   # Create train data:
-  python generate_tfrecord.py --csv_input=data/train_labels.csv  --output_path=train.record
+  python3 generate_tfrecord.py --csv_input=data/train_labels.csv  --output_path=data/train.record
+
   # Create test data:
-  python generate_tfrecord.py --csv_input=data/test_labels.csv  --output_path=test.record
+  python3 generate_tfrecord.py --csv_input=data/test_labels.csv  --output_path=data/test.record
 """
 from __future__ import division
 from __future__ import print_function
@@ -19,7 +20,7 @@ from PIL import Image
 from object_detection.utils import dataset_util
 from collections import namedtuple, OrderedDict
 
-flags = tf.app.flags
+flags = tf.compat.v1.app.flags
 flags.DEFINE_string('csv_input', '', 'Path to the CSV input')
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
 flags.DEFINE_string('image_dir', '', 'Path to images')
@@ -29,15 +30,15 @@ FLAGS = flags.FLAGS
 # TO-DO replace this with label map
 def class_text_to_int(row_label):
     if row_label == 'Horse':
-        return 1
+      return 1
     elif row_label == 'Cow':
-        return 2
+      return 2
     elif row_label == 'Sheep':
-        return 3
+      return 3
     elif row_label == 'Person':
-        return 4
+      return 4
     else:
-        None
+      None
 
 
 def split(df, group):
@@ -47,13 +48,13 @@ def split(df, group):
 
 
 def create_tf_example(group, path):
-    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+    with tf.io.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
         encoded_jpg = fid.read()
     encoded_jpg_io = io.BytesIO(encoded_jpg)
     image = Image.open(encoded_jpg_io)
     width, height = image.size
 
-    filename = group.filename.encode('utf8')
+    filename = group.filename.encode('utf-8')
     image_format = b'jpg'
     xmins = []
     xmaxs = []
@@ -88,7 +89,7 @@ def create_tf_example(group, path):
 
 
 def main(_):
-    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
+    writer = tf.compat.v1.python_io.TFRecordWriter(FLAGS.output_path)
     path = os.path.join(FLAGS.image_dir)
     examples = pd.read_csv(FLAGS.csv_input)
     grouped = split(examples, 'filename')
@@ -102,4 +103,4 @@ def main(_):
 
 
 if __name__ == '__main__':
-    tf.app.run()
+    tf.compat.v1.app.run()
